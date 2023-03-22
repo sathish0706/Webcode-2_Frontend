31a02f6e404b9b01a1389c2ecb249a95cdc99907
Sathish R <107674282+sathish0706@users.noreply.github.com>
2023-03-22 08:09:45 +0000

    Update AddToCart.js

--- src/Components/AddToCart.js
+++ src/Components/AddToCart.js
@@ -8,6 +8,7 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { useState } from "react";
 import AddressDetails from "./AddressDetails";
+
 // import axios from 'axios';
 
 function AddToCart({ cart, setCart, setCount, count }) {
@@ -25,43 +26,52 @@ function AddToCart({ cart, setCart, setCount, count }) {
   // };
 
   return (
-    <>
+    <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>pizza image</TableCell>
-            <TableCell align="left">pizza name</TableCell>
-            <TableCell align="left">Remove</TableCell>
-            <TableCell align="right">price</TableCell>
+            <TableCell>
+              <h2>pizza image</h2>
+            </TableCell>
+            <TableCell>
+              <h2>pizza name</h2>
+            </TableCell>
+            <TableCell align="center">
+              <h2>Remove</h2>
+            </TableCell>
+            <TableCell>
+              <h2>price</h2>
+            </TableCell>
           </TableRow>
         </TableHead>
-      </Table>
-      {cart.map((cr, i) => (
-        <Cart
-          key={i}
-          cart={cr}
-          id={cr.id}
-          setCart={setCart}
-          crt={cart}
-          setCount={setCount}
-          setTotalPrice={setTotalPrice}
-          totalPrice={totalPrice}
-          count={count}
-        />
-      ))}
 
-      {order ? (
-        <AddressDetails totalAmount={totalAmount} />
-      ) : (
-        <Button
-          variant="contained"
-          color="success"
-          onClick={() => setOrder(!order)}
-        >
-          Place Order &nbsp;&nbsp; ₹{totalAmount}
-        </Button>
-      )}
-    </>
+        {cart.map((cr, i) => (
+          <Cart
+            key={i}
+            cart={cr}
+            id={cr.id}
+            setCart={setCart}
+            crt={cart}
+            setCount={setCount}
+            setTotalPrice={setTotalPrice}
+            totalPrice={totalPrice}
+            count={count}
+          />
+        ))}
+
+        {order ? (
+          <AddressDetails totalAmount={totalAmount} />
+        ) : (
+          <Button
+            variant="contained"
+            color="success"
+            onClick={() => setOrder(!order)}
+          >
+            Place Order &nbsp;&nbsp; ₹{totalAmount}
+          </Button>
+        )}
+      </Table>
+    </TableContainer>
   );
 }
 
@@ -75,10 +85,6 @@ function Cart({
   setTotalPrice,
   totalPrice,
 }) {
-  if (cart.price > 1) {
-    totalPrice.push(cart.price);
-  }
-
   const handleDelete = async (prce) => {
     // setTotalPrice(prce);
     let delt = crt.filter((deltData) => deltData.id !== id);
@@ -86,39 +92,41 @@ function Cart({
     setCount(count - 1);
     totalPrice.push(-prce);
   };
+
+  if (cart.price > 1) {
+    console.log(cart.price);
+    totalPrice.push(cart.price);
+  }
+
   // const totalPrice = Price.reduce((x, i) => x + i, 0);
   //
   // console.log(totalPrice);
 
   return (
     <>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableBody>
-            <TableRow>
-              <TableCell component="th" scope="row">
-                <img className="cart-img" src={cart.img} alt={cart.name} />
-              </TableCell>
-              <TableCell align="left">
-                <b>{cart.name}</b>
-              </TableCell>
-              <TableCell align="right"></TableCell>
-              <TableCell align="left">
-                <Button
-                  variant="contained"
-                  color="error"
-                  id="cart"
-                  onClick={() => handleDelete(cart.price)}
-                >
-                  Remove from cart
-                </Button>
-              </TableCell>
-              <TableCell align="right">{cart.price}</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <TableBody>
+        <TableRow>
+          <TableCell align="left">
+            <img className="cart-img" src={cart.img} alt={cart.name} />
+          </TableCell>
+          <TableCell align="left">
+            <b>{cart.name}</b>
+          </TableCell>
+          <TableCell align="center">
+            <Button
+              variant="contained"
+              color="error"
+              id="cart"
+              onClick={() => handleDelete(cart.price)}
+            >
+              Remove from cart
+            </Button>
+          </TableCell>
+          <TableCell align="left">{cart.price}</TableCell>
+        </TableRow>
+      </TableBody>
     </>
   );
 }
 export default AddToCart;
+
